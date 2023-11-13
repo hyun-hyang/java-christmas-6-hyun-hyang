49d038fe1b5777600cca3496f8cd1c785aafc077
hyun-hyang <jimin3060@naver.com>
2023-11-14 06:30:32 +0900

    feat: 패키지 생성 및 컨트롤러의 생성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,9 @@
 package christmas;
 
+import controller.DecemberEventPlanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new DecemberEventPlanner();
     }
 }
